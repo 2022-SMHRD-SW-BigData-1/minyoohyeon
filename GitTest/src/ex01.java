cfd341c27580daf1a6e76bc9fa5d48380444d16a
EGEG22A07 <EGEG22A07@DESKTOP-LV5L344>
2022-08-31 10:24:59 +0900

    두번째 커밋!!

--- GitTest/src/ex01.java
+++ GitTest/src/ex01.java
@@ -5,6 +5,9 @@ public class ex01 {
 		// TODO Auto-generated method stub
 
 		System.out.println("첫번째 커밋!!");
+		// 두번째 커밋 출력문 만든 뒤 --> 해당 내용을 깃허브 사이트로 올려주세요.(push)
+		
+		System.out.println("두번째 커밋!!");
 		
 	}
 
